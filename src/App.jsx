f715656ec7fa03654a14ad0a7a760baeecf22fcc
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-19 18:38:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Texto2 from './components/Texto2'
+
+function App() {
+
+  return (
+    <>
+      <Texto2 nome="Livea"/>
+      <Texto2 nome="Obama"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Texto2.jsx
@@ -0,0 +1,6 @@
+function Texto2(props){
+    
+    return <h1>Olá,{props.nome}!</h1>
+}
+
+export default Texto2
